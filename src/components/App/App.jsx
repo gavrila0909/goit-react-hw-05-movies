2b73e558ff811c11ff33e5f1385196552a23205b
gavrila0909 <gavriladanna@gmail.com>
2024-05-29 14:47:53 +0100

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,31 +1,30 @@
 import { Routes, Route } from 'react-router-dom';
-import { lazy } from "react";
+import { lazy, Suspense } from 'react';
 import SharedLayout from '../SharedLayout/SharedLayout';
+import Loader from 'components/Loader/Loader';
 
-const Home = lazy(() => import("../../pages/Home/Home"));
-const Movies = lazy(() => import("../../pages/Movies/Movies"));
-const MovieDetails = lazy(() => import("../../pages/MovieDetails/MovieDetails"));
-const NotFound = lazy(() => import("../../pages/NotFound/NotFound"));
-const Cast = lazy(() => import("../Cast/Cast"));
-const Reviews = lazy(() => import("../Reviews/Reviews"));
-
+const Home = lazy(() => import('../../pages/Home/Home'));
+const Movies = lazy(() => import('../../pages/Movies/Movies'));
+const MovieDetails = lazy(() => import('../../pages/MovieDetails/MovieDetails'));
+const NotFound = lazy(() => import('../../pages/NotFound/NotFound'));
+const Cast = lazy(() => import('../Cast/Cast'));
+const Reviews = lazy(() => import('../Reviews/Reviews'));
 
 export const App = () => {
   return (
-    <>
-      <section>
+    <section>
+      <SharedLayout />
+      <Suspense fallback={<div><Loader /></div>}>
         <Routes>
-          <Route path="/" element={<SharedLayout />}>
-            <Route index element={<Home />} />
-            <Route path="/movies" element={<Movies />} />
-            <Route path="/movies/:movieId" element={<MovieDetails />}>
-              <Route path="cast" element={<Cast />} />
-              <Route path="reviews" element={<Reviews />} />
-            </Route>
-            <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
+          <Route path="*" element={<NotFound />} />
         </Routes>
-      </section>
-    </>
+      </Suspense>
+    </section>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,11 +7,9 @@ import styles from './Home.module.css';
 
 const Home = () => {
   const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function fetchMovies() {
-      setIsLoading(true);
       try {
         const response = await getMovies();
         //console.log(response.results);
@@ -19,7 +17,6 @@ const Home = () => {
       } catch (error) {
         console.error('Error fetching trending movies:', error);
       } finally {
-        setIsLoading(false);
       }
     }
     fetchMovies();
@@ -27,26 +24,19 @@ const Home = () => {
 
   return (
     <div className={styles.trendingContainer}>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <>
-          <h1>Trending movies today</h1>
-          <ul>
-            {movies.map(movie => (
-              <li key={movie.id}>
-                <Link to={`/movies/${movie.id}`}>
-                  <img
-                    src={IMAGE_URL + movie.backdrop_path}
-                    alt={movie.title}
-                  />
-                  <p>{movie.title}</p>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </>
-      )}
+      <>
+        <h1>Trending movies today</h1>
+        <ul>
+          {movies.map(movie => (
+            <li key={movie.id}>
+              <Link to={`/movies/${movie.id}`}>
+                <img src={IMAGE_URL + movie.backdrop_path} alt={movie.title} />
+                <p>{movie.title}</p>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </>
     </div>
   );
 };
